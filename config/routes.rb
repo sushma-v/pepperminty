4a10246801dde153d40d3daa019e4cc6ee24a40f
Sushma Venugopal <sushma.venugopal@smarta.com>
2018-09-03 11:45:32 +0100

    Implement home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  root to: 'visitors#index'
+  root to: 'pages#home'
+
+  get '/home', to: 'pages#home', as: ''
+  get '/about', to: 'pages#about', as: 'about'
+  get '/contact', to: 'pages#contact', as: 'contact'
+
 end
