e39871c1f1a7e8292865777605f79fa05716c050
Sushma Venugopal <sushma.venugopal@smarta.com>
2018-09-07 11:52:31 +0100

    Edit height of select tag

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,5 +6,10 @@ class PagesController < ApplicationController
   end
 
   def contact
+    @categories = [["Website Design & Development", 1],
+                   ["Search Engine Optimisation", 2],
+                   ["Advertising", 3],
+                   ["Social Media", 4],
+                   ["Others", 5]]
   end
 end
