3fa1cd6115c7337a57da5f833e5a2ba49b08b4fc
Sushma Venugopal <sushma.venugopal@smarta.com>
2018-09-07 12:16:59 +0100

    Whitelist contact form params

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -14,9 +14,15 @@ class PagesController < ApplicationController
   end
 
   def submit_query
-    Rails.logger.info("params #{params.to_json}")
+    Rails.logger.info("params #{query_params.to_json}")
 
     flash[:notice] = "Your Query has been submitted, we will be in touch with you shortly"
     redirect_to contact_path
+  end
+
+  private
+
+  def query_params
+    params.permit(:name, :email, :company_name, :phone, :category, :message)
   end
 end
