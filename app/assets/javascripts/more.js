689fb1b9a847409e964d1e05f82516154e12d31d
Sushma Venugopal <sushma.venugopal@smarta.com>
2018-10-25 09:43:57 +0100

    Configure to serve static files

--- app/assets/javascripts/more.js
+++ app/assets/javascripts/more.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
     $('html, body').animate({scrollTop: 0}, "slow");
 
-    $(".more-home-arrow").click(function(){ console.log("hehe");
+    $(".more-home-arrow").click(function(){ console.log("test");
         var position = $('#more-home').offset();
         $('html, body').animate({scrollTop: position.top}, "slow");
     });
