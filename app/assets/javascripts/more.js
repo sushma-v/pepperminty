35098716c9678bd2869a37180599c388b22482b6
Sushma Venugopal <sushma.venugopal@smarta.com>
2018-10-25 09:30:10 +0100

    Place a console message to check the arrow click

--- app/assets/javascripts/more.js
+++ app/assets/javascripts/more.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
     $('html, body').animate({scrollTop: 0}, "slow");
 
-    $(".more-home-arrow").click(function(){
+    $(".more-home-arrow").click(function(){ console.log("hehe");
         var position = $('#more-home').offset();
         $('html, body').animate({scrollTop: position.top}, "slow");
     });
