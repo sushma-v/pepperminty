a8e54acd7aef3320784b26fad3a6b53b3ed85049
Sushma Venugopal <sushma.venugopal@smarta.com>
2018-10-25 10:36:19 +0100

    Precompile assets in prod

---
+++ public/assets/more-018d7ea5a09f70044cbafa52cce5de561d61bc1c3ccf31da55c77e2a65cebe2d.js
@@ -0,0 +1,1 @@
+$(document).ready(function(){$("html, body").animate({scrollTop:0},"slow"),$(".more-home-arrow").click(function(){console.log("test");var o=$("#more-home").offset();$("html, body").animate({scrollTop:o.top},"slow")})});
