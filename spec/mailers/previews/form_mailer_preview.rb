193d3e6f860dd5cccb2f77343ba322353ef8d122
Sushma Venugopal <sushma.venugopal@smarta.com>
2018-10-29 16:00:28 +0000

    Implement the mailer layout

--- spec/mailers/previews/form_mailer_preview.rb
+++ spec/mailers/previews/form_mailer_preview.rb
@@ -3,7 +3,9 @@ class FormMailerPreview < ActionMailer::Preview
 
   # Preview this email at http://localhost:3000/rails/mailers/form_mailer/contact
   def contact
-    FormMailer.contact
+    params = {name: "John Doe", email: "john.doe@example.com", company_name:"Company Name", phone:"07440090909",
+              category:"SEO", message: "Message goes here"}
+    FormMailer.contact(params)
   end
 
 end
