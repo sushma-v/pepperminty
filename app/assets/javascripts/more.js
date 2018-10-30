76bae9e06f4c031a613bfafd46ba012557dcd335
Sushma Venugopal <sushma.venugopal@smarta.com>
2018-10-30 10:46:56 +0000

    Link all contact buttons to contact us section in the page

--- app/assets/javascripts/more.js
+++ app/assets/javascripts/more.js
@@ -1,8 +1,13 @@
 $(document).ready(function(){
     $('html, body').animate({scrollTop: 0}, "slow");
 
-    $(".more-home-arrow").click(function(){ console.log("test");
+    $(".more-home-arrow").click(function(){
         var position = $('#more-home').offset();
+        $('html, body').animate({scrollTop: position.top}, "slow");
+    });
+
+    $(".start").click(function(){
+        var position = $('.contact-bg').offset();
         $('html, body').animate({scrollTop: position.top}, "slow");
     });
 });
