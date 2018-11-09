58821976bae18fd81ac9ddd9161cded8e457fb2d
Sushma Venugopal <sushma.venugopal@smarta.com>
2018-11-09 16:11:55 +0000

    Display packages for social media page

--- app/assets/javascripts/more.js
+++ app/assets/javascripts/more.js
@@ -16,6 +16,16 @@ $(document).ready(function(){
         $('html, body').animate({scrollTop: position.top}, "slow");
     });
 
+    $('.website-packages').on('shown.bs.collapse', function () {
+        var position = $('.website').offset();
+        $('html, body').animate({scrollTop: position.top}, "slow");
+    });
+
+    $('.website-packages').on('hide.bs.collapse', function () {
+        var position = $('.website').prev().offset();
+        $('html, body').animate({scrollTop: position.top}, "5000");
+    });
+
     $('.seo-packages').on('shown.bs.collapse', function () {
         var position = $('.seo').offset();
         $('html, body').animate({scrollTop: position.top}, "slow");
@@ -23,6 +33,26 @@ $(document).ready(function(){
 
     $('.seo-packages').on('hide.bs.collapse', function () {
         var position = $('.seo').prev().offset();
+        $('html, body').animate({scrollTop: position.top}, "5000");
+    });
+
+    $('.search-packages').on('shown.bs.collapse', function () {
+        var position = $('.search').offset();
+        $('html, body').animate({scrollTop: position.top}, "slow");
+    });
+
+    $('.search-packages').on('hide.bs.collapse', function () {
+        var position = $('.search').prev().offset();
+        $('html, body').animate({scrollTop: position.top}, "5000");
+    });
+
+    $('.social-packages').on('shown.bs.collapse', function () {
+        var position = $('.social').offset();
+        $('html, body').animate({scrollTop: position.top}, "slow");
+    });
+
+    $('.social-packages').on('hide.bs.collapse', function () {
+        var position = $('.social').prev().offset();
         $('html, body').animate({scrollTop: position.top}, "5000");
     });
 });
