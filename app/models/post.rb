50ece583763dd78fc67de7eb2db9dc81789a51d4
Sushma Venugopal <sushma.venugopal@smarta.com>
2019-01-30 09:34:48 +0000

    Create PostHashTag model and create a has_many:through association

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,7 @@
 class Post < ApplicationRecord
+  has_many :post_hash_tags
+  has_many :hash_tags, through: :post_hash_tags
+
   enum status: [:draft, :published]
 
   validates :title, presence: true
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190130083708) do
+ActiveRecord::Schema.define(version: 20190130092634) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,15 @@ ActiveRecord::Schema.define(version: 20190130083708) do
     t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "post_hash_tags", force: :cascade do |t|
+    t.bigint "post_id"
+    t.bigint "hash_tag_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["hash_tag_id"], name: "index_post_hash_tags_on_hash_tag_id"
+    t.index ["post_id"], name: "index_post_hash_tags_on_post_id"
   end
 
   create_table "posts", force: :cascade do |t|
