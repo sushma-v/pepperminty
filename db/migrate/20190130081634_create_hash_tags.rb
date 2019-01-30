22b1817c4dad37c697373ba8fafb9f07608e4c32
Sushma Venugopal <sushma.venugopal@smarta.com>
2019-01-30 08:22:34 +0000

    Create model HashTag

---
+++ db/migrate/20190130081634_create_hash_tags.rb
@@ -0,0 +1,9 @@
+class CreateHashTags < ActiveRecord::Migration[5.1]
+  def change
+    create_table :hash_tags do |t|
+      t.string :name, null: false
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190129132205) do
+ActiveRecord::Schema.define(version: 20190130081634) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,12 @@ ActiveRecord::Schema.define(version: 20190129132205) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["type"], name: "index_ckeditor_assets_on_type"
+  end
+
+  create_table "hash_tags", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "posts", force: :cascade do |t|
