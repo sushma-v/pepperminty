8fc09e996d329a44593131ab840da1b90d25a94a
Sushma Venugopal <sushma.venugopal@smarta.com>
2019-01-30 05:38:49 +0000

    Implement meta desc and meta keywords for post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -71,6 +71,6 @@ class PostsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def post_params
-      params.require(:post).permit(:title, :content, :main_image, :alt_text, :category, :author, :status)
+      params.require(:post).permit(:title, :content, :main_image, :alt_text, :category, :meta_description, :meta_keywords, :author, :status)
     end
 end
