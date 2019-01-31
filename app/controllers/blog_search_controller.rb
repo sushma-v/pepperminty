be263e199e2136766a7242be5243f4297b8a19f9
Sushma Venugopal <sushma.venugopal@smarta.com>
2019-01-31 09:16:05 +0000

    Implement searchable hash tags

--- app/controllers/blog_search_controller.rb
+++ app/controllers/blog_search_controller.rb
@@ -1,5 +1,6 @@
 class BlogSearchController < ApplicationController
   def index
+    Rails.logger.info("======1======="+params.to_json)
     query = params[:query].start_with?('#') ? params[:query].gsub('#', '') : params[:query]
     @posts = Post.joins(:hash_tags).where(hash_tags: {name: query})
     #@posts = Post.where("content like ?", "%#{params[:query]}%") #search by desc
--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,11 @@
 module PostsHelper
+  def render_with_hashtags(content)
+    content.gsub(/#\w+/) { |word|
+      link_to word, blog_search_path(query: word)
+    }
+  end
+
+  def escape_html(content)
+    CGI.escape_html(content)
+  end
 end
