bf217d5543c7572182f9563ff6e35e4365ed090b
Sushma Venugopal <sushma.venugopal@smarta.com>
2019-02-01 09:52:41 +0000

    Implement styles for the search and category selection

--- app/controllers/blog_search_controller.rb
+++ app/controllers/blog_search_controller.rb
@@ -3,4 +3,8 @@ class BlogSearchController < ApplicationController
     query = params.fetch(:tag, 'not_found') == 'not_found' ? params.fetch(:query, '') : params.fetch(:tag, '')
     @posts = Post.joins(:hash_tags).where(hash_tags: {name: query.delete('#')})
   end
+
+  def by_category
+
+  end
 end
