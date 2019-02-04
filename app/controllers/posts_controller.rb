c1bc7ab481179efc6ecd70e17d69acddea030c80
Sushma Venugopal <sushma.venugopal@smarta.com>
2019-02-04 06:52:00 +0000

    Edit the Category selection in views

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -72,6 +72,6 @@ class PostsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def post_params
-      params.require(:post).permit(:title, :content, :main_image, :alt_text, :category, :meta_description, :meta_keywords, :author, :status)
+      params.require(:post).permit(:title, :content, :main_image, :alt_text, :category_id, :meta_description, :meta_keywords, :author, :status)
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190204055609) do
+ActiveRecord::Schema.define(version: 20190204061823) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,7 +55,6 @@ ActiveRecord::Schema.define(version: 20190204055609) do
     t.string "title", null: false
     t.text "content", null: false
     t.string "main_image"
-    t.string "category", null: false
     t.string "author", null: false
     t.integer "status", default: 0
     t.datetime "created_at", null: false
