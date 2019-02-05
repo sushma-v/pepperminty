3e939412d5e145cadd65bf3836e4b5c3bfa5de75
Sushma Venugopal <sushma.venugopal@smarta.com>
2019-02-05 07:05:54 +0000

    Implement create categories in index view

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -5,6 +5,7 @@ class CategoriesController < ApplicationController
   # GET /categories.json
   def index
     @categories = Category.all
+    @category = Category.new
   end
 
   # GET /categories/1
@@ -25,15 +26,10 @@ class CategoriesController < ApplicationController
   # POST /categories.json
   def create
     @category = Category.new(category_params)
-
-    respond_to do |format|
-      if @category.save
-        format.html { redirect_to @category, notice: 'Category was successfully created.' }
-        format.json { render :show, status: :created, location: @category }
-      else
-        format.html { render :new }
-        format.json { render json: @category.errors, status: :unprocessable_entity }
-      end
+    if @category.save
+      redirect_to categories_url, notice: 'Category was successfully created.'
+    else
+      render :index
     end
   end
 
