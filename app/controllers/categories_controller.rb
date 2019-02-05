7c72690d8b0c7db26342e91f50574de6d68d6084
Sushma Venugopal <sushma.venugopal@smarta.com>
2019-02-05 09:32:08 +0000

    Update parent_id for a record

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -58,6 +58,6 @@ class CategoriesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def category_params
-      params.require(:category).permit(:name, :display)
+      params.require(:category).permit(:name, :display, :parent_id)
     end
 end
