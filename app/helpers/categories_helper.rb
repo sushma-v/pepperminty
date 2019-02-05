149523e376d43fb2669418826d1fbd324363e9eb
Sushma Venugopal <sushma.venugopal@smarta.com>
2019-02-05 08:33:57 +0000

    Implement helper method to display category tree

--- app/helpers/categories_helper.rb
+++ app/helpers/categories_helper.rb
@@ -1,2 +1,12 @@
 module CategoriesHelper
+  def nested_attributes(attributes)
+    content_tag :ul do
+      attributes.each do |attribute|
+        concat(content_tag(:li) { link_to(attribute.name, '#') })
+        if attribute.has_children?
+          concat(nested_attributes(attribute.children))
+        end
+      end
+    end
+  end
 end
