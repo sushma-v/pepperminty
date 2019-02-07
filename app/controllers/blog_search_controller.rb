90b9d98154bb285b4af060106ca0c3fd1cd44ebe
Sushma Venugopal <sushma.venugopal@smarta.com>
2019-02-07 09:19:02 +0000

    Implement pagination for search blog pages

--- app/controllers/blog_search_controller.rb
+++ app/controllers/blog_search_controller.rb
@@ -1,7 +1,7 @@
 class BlogSearchController < ApplicationController
   def index
     query = params.fetch(:tag, 'not_found') == 'not_found' ? params.fetch(:query, '') : params.fetch(:tag, '')
-    @posts = Post.joins(:hash_tags).where(hash_tags: {name: query.delete('#')})
+    @posts = Post.joins(:hash_tags).where(hash_tags: {name: query.delete('#')}).paginate(:page => params[:page]).order('id DESC')
   end
 
   def by_category
@@ -11,6 +11,6 @@ class BlogSearchController < ApplicationController
     descendant_ids = category.descendant_ids
     descendant_ids << category.id
 
-    @posts = Post.where(category_id: descendant_ids)
+    @posts = Post.where(category_id: descendant_ids).paginate(:page => params[:page]).order('id DESC')
   end
 end
