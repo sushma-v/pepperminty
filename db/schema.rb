0442cd2a64cdf139b287604af61a96992868e034
Sushma Venugopal <sushma.venugopal@smarta.com>
2019-02-08 10:59:40 +0000

    remove author from posts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190208103657) do
+ActiveRecord::Schema.define(version: 20190208105830) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,7 +55,6 @@ ActiveRecord::Schema.define(version: 20190208103657) do
     t.string "title", null: false
     t.text "content", null: false
     t.string "main_image"
-    t.string "author", null: false
     t.integer "status", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
