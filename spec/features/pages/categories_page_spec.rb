b160b551973f2080c79270db66ea96dd07039d60
Sushma Venugopal <sushma.venugopal@smarta.com>
2019-02-11 12:57:19 +0000

    Test the create and destroy methods of categories cotroller

--- spec/features/pages/categories_page_spec.rb
+++ spec/features/pages/categories_page_spec.rb
@@ -6,6 +6,31 @@ feature 'Categories page' do
 
     signup("test@example.com", "abcd1234")
 
+    visit categories_path
+    expect(page).to have_content "Tree"
+
+    #create
+    page.fill_in 'category_name', with: 'sales'
+    page.select "Root", :from => "category[parent_id]"
+    page.check 'category_display'
+    page.click_button 'Create Category'
+
+    expect(page).to have_content 'Category was successfully created.'
+
+    #edit
+    #within('.tab-content') do
+    #  page.all('a')[0].click
+    #end
+    #page.fill_in 'category_name', with: 'online sales'
+    #page.click_button 'Update Category'
+    #expect(page).to have_content 'Category was successfully updated.'
+
+    #delete
+    within('.tab-content') do
+      page.all('a')[1].click
+    end
+    expect(page).to have_content 'Category was successfully destroyed.'
+
   end
 
 end
