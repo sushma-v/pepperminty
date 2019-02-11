ba1b47460c44b6f7003fe9553e0a764b989a5e7a
Sushma Venugopal <sushma.venugopal@smarta.com>
2019-02-11 11:02:39 +0000

    Test the contact form

--- spec/mailers/form_mailer_spec.rb
+++ spec/mailers/form_mailer_spec.rb
@@ -2,16 +2,19 @@ require "rails_helper"
 
 RSpec.describe FormMailer, type: :mailer do
   describe "contact" do
-    let(:mail) { FormMailer.contact }
+    let(:params) { {name: 'John Doe', company_name: 'JD Limited', email: 'john@pm.co.uk', phone: '1111100000',
+                    category: 'SEO', message: 'Please provide info on SEO?'} }
+    let(:mail) { FormMailer.contact(params) }
 
     it "renders the headers" do
-      expect(mail.subject).to eq("Contact")
-      expect(mail.to).to eq(["to@example.org"])
-      expect(mail.from).to eq(["from@example.com"])
+      expect(mail.subject).to eq("Pepperminty Limited: Contact Form")
+      expect(mail.to).to eq(["info@pepperminty.co.uk"])
+      expect(mail.from).to eq( Array(params[:email]) )
     end
 
     it "renders the body" do
-      expect(mail.body.encoded).to match("Hi")
+      expect(mail.body.encoded).to match("Contact Form details")
+      expect(mail.body.encoded).to match("Message: Please provide info on SEO?")
     end
   end
 
