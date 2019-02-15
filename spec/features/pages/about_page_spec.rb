a4369773512d24ac1e45029372ed4e99ea8be07e
Sushma Venugopal <sushma.venugopal@smarta.com>
2019-02-15 08:30:35 +0000

    Remove comments from feature tests

--- spec/features/pages/about_page_spec.rb
+++ spec/features/pages/about_page_spec.rb
@@ -1,13 +1,5 @@
-# Feature: 'About' page
-#   As a visitor
-#   I want to visit an 'services' page
-#   So I can learn more services the website
 feature 'About page' do
 
-  # Scenario: Visit the 'services' page
-  #   Given I am a visitor
-  #   When I visit the 'services' page
-  #   Then I see "About the Website"
   scenario 'Visit the services page' do
     visit about_path
     expect(page).to have_content 'Our Aim'
