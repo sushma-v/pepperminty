07a9d7459950ec3d720ab1c7217ab5ef1a86d699
Sushma Venugopal <sushma.venugopal@smarta.com>
2019-02-18 09:35:07 +0000

    Remove commented code from config file

--- app/assets/javascripts/ckeditor/config.js
+++ app/assets/javascripts/ckeditor/config.js
@@ -1,36 +1,3 @@
-/*CKEDITOR.editorConfig = function( config ) {
-    // config.skin = 'moona-lisa';
-    // Define changes to default configuration here. For example:
-    // config.language = 'fr';
-    // config.extraPlugins = 'font';
-    config.uiColor = '#FF738E';
-    config.toolbar_Full =
-        [
-            { name: 'document', items : [ 'Source','-','Save','NewPage','DocProps','Preview','Print','-','Templates' ] },
-            { name: 'clipboard', items : [ 'Cut','Copy','Paste','PasteText','PasteFromWord','-','Undo','Redo' ] },
-            { name: 'editing', items : [ 'Find','Replace','-','SelectAll','-','SpellChecker', 'Scayt' ] },
-            { name: 'forms', items : [ 'Form', 'Checkbox', 'Radio', 'TextField', 'Textarea', 'Select', 'Button', 'ImageButton',
-                'HiddenField' ] },
-            '/',
-            { name: 'basicstyles', items : [ 'Bold','Italic','Underline','Strike','Subscript','Superscript','-','RemoveFormat' ] },
-            { name: 'paragraph', items : [ 'NumberedList','BulletedList','-','Outdent','Indent','-','Blockquote','CreateDiv',
-                '-','JustifyLeft','JustifyCenter','JustifyRight','JustifyBlock','-','BidiLtr','BidiRtl' ] },
-            { name: 'links', items : [ 'Link','Unlink','Anchor' ] },
-            { name: 'insert', items : [ 'Image','Flash','Table','HorizontalRule','Smiley','SpecialChar','PageBreak','Iframe' ] },
-            '/',
-            { name: 'styles', items : [ 'Styles','Format','Font','FontSize' ] },
-            { name: 'colors', items : [ 'TextColor','BGColor' ] },
-            { name: 'tools', items : [ 'Maximize', 'ShowBlocks','-','About' ] }
-        ];
-
-    config.toolbar_Basic =
-        [
-            ['Bold', 'Italic', '-', 'NumberedList', 'BulletedList', '-', 'Link', 'Unlink','-','About']
-        ];
-    config.toolbar = 'Full';
-};*/
-
-
 CKEDITOR.editorConfig = function( config )
 {
     // Define changes to default configuration here. For example:
