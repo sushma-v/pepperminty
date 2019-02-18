2812d0c7298eb78f923f839717dc5e854b1e24f6
Sushma Venugopal <sushma.venugopal@smarta.com>
2019-02-18 10:06:32 +0000

    Edit the ckeditor autoload path

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -13,4 +13,4 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # folder are already added.
 Rails.application.config.assets.precompile += %w(*.js)
 Rails.application.config.assets.precompile += %w( ckeditor/* )
-Rails.application.config.autoload_paths += %W(#{config.root}/app/models/ckeditor)
+Rails.application.config.autoload_paths += %W(../app/models/ckeditor)
