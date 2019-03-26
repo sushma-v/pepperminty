a6bf19a70c4fbe2b5d8fd18b8b3df1dfa96f986b
Sushma Venugopal <sushma.venugopal@smarta.com>
2019-03-26 09:21:37 +0000

    Add image titles to services page images

--- app/assets/javascripts/seo.js
+++ app/assets/javascripts/seo.js
@@ -6,4 +6,10 @@ $(document).ready(function(){
     $('.social-block-bg').attr('title','Pepperminty_marketing_services_social_media');
     $('.show-bgimg').attr('title','Pepperminty_marketing_revolutionise_your_business');
     $('.contactus-bg').attr('title','Pepperminty_digital_marketing_agency_contact_us');
+
+    $('.about-bgimg').attr('title','Pepperminty_Digital_Marketing_About_Us');
+    $('.website-show').attr('title','Pepperminty_marketing_website_kickstart');
+    $('.seo-show').attr('title','Pepperminty_marketing_seo_kickstart');
+    $('.paid-search-show').attr('title','Pepperminty_marketing_ppc_kickstart');
+    $('.social-show').attr('title','Pepperminty_marketing_social_media_kickstart');
 });
