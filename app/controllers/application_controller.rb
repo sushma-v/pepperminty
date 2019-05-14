af5eb5b23ef3d34f28fde865081fe3990ba21bf0
Sushma Venugopal <sushma.venugopal@smarta.com>
2019-05-14 12:02:20 +0100

    Remove unused action from application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,3 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-
-  before_action :show_flash
-
-  private
-
-  def show_flash
-    #flash.now[:notice] = "Found the services page!" if request.path == '/pages/services'
-  end
 end
