ea6b0070c7f40417117a41d5901a84f0eda4bc08
Sushma Venugopal <sushma.venugopal@smarta.com>
2019-05-15 10:18:32 +0100

    Refatcor the blog concern

--- app/controllers/concerns/blog_concern.rb
+++ app/controllers/concerns/blog_concern.rb
@@ -2,7 +2,9 @@ module BlogConcern
   extend ActiveSupport::Concern
 
   def self.upload_image(main_image)
-    Cloudinary::Uploader.upload(main_image, {folder: "/pepperminty/#{Rails.env}/blog/",
+    folder = "/pepperminty/#{Rails.env}/blog/"
+
+    Cloudinary::Uploader.upload(main_image, {folder: folder,
                                              use_filename: true})
   end
 end
