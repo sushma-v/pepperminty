62661c4b6de39a5bb016a12c4efe88bcfd14cb5f
Sushma Venugopal <sushma.venugopal@smarta.com>
2019-05-15 11:11:31 +0100

    Refactor display main image to a hepler method

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -10,4 +10,12 @@ module PostsHelper
   def escape_html(content)
     CGI.escapeHTML(content)
   end
+
+  def display_main_image(post)
+    if post.main_image.empty?
+      cl_image_tag Post.first.main_image, alt: post.alt_text rescue nil
+    else
+      cl_image_tag post.main_image, alt: post.alt_text rescue nil
+    end
+  end
 end
